28bc1e24ef9c27a5bfc09fb8e16fef143d5d06b2
Michael Kim <michaelkim009@gmail.com>
2023-09-11 17:09:49 -0700

    created separate input.mjs to hold input JSON

---
+++ index.mjs
@@ -0,0 +1,4 @@
+import input from "./input.mjs";
+
+
+console.log(JSON.stringify(input, null, 2));
---
+++ input.mjs
@@ -0,0 +1,31 @@
+const input = {
+    "team": [
+        {
+            "name": "Eddie",
+            "attributes": {
+                "intelligence": 1,
+                "strength": 5,
+                "endurance": 3,
+                "spicyFoodTolerance": 1,
+            }
+        }, {
+            "name": "Will",
+            "attributes": {
+                "intelligence": 9,
+                "strength": 4,
+                "endurance": 1,
+                "spicyFoodTolerance": 6,
+            }
+        }, {
+            "name": "Mike",
+            "attributes": {
+                "intelligence": 3,
+                "strength": 2,
+                "endurance": 9,
+                "spicyFoodTolerance": 5,
+            }
+        }
+    ]
+};
+
+export default input;
